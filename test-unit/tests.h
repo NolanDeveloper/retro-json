78058759d6adfbf842f9f4d89e85bb814e674d99
Denis M <nolane16@gmail.com>
2020-11-05 21:32:54 +0300

    Add header guards into test-unit

--- test-unit/tests.h
+++ test-unit/tests.h
@@ -1,3 +1,8 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+#include <stdbool.h>
+
 bool test_array(void);
 bool test_object(void);
 bool test_number(void);
@@ -5,3 +10,5 @@ bool test_utf(void);
 bool test_string(void);
 bool test_parser(void);
 bool test_pretty_printer(void);
+
+#endif
